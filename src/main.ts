431a3ae1d862066180b908367e00034b64e1ec4d
sqlnice <472252510@qq.com>
2021-01-12 23:27:49 +0800

    增加element-plus；按需引入

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'element-plus/lib/theme-chalk/index.css';
 
 createApp(App)
   .use(store)
   .use(router)
-  .mount("#app");
+  .mount('#app');
