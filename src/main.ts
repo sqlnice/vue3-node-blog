0a6298517c0cd3639f7f558e2a3a81163bffb154
sqlnice <472252510@qq.com>
2021-01-15 00:11:21 +0800

    项目初始化，删除增加一些页面

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
 import 'element-plus/lib/theme-chalk/index.css';
-
+import '@/styles/reset.less';
 createApp(App)
   .use(store)
   .use(router)
   .mount('#app');
+78;
